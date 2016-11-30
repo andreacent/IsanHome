63c5b553d72c98aaf299a4bcc23482a0c69bd723
andreacent <andreacent8@gmail.com>
2016-11-30 00:21:34 -0400

    edit profile, add alarm and privacity

--- hamltohtml.sh
+++ hamltohtml.sh
@@ -5,3 +5,7 @@ haml haml/water.haml water.html
 haml haml/light.haml light.html
 haml haml/schedules.haml schedules.html
 haml haml/account.haml account.html
+haml haml/edit-profile.haml edit-profile.html
+haml haml/apphelp.haml apphelp.html
+haml haml/privacity.haml privacity.html
+haml haml/add-alarm.haml add-alarm.html
